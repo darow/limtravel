dfafc466dd95277f3370e238576d81bbedcad32e
darow <xerosyv@gmail.com>
2019-08-07 09:51:23 +0300

    pre-release

--- main_page/urls.py
+++ main_page/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name="main_page"),
+    path('tourist_visas', views.tourist_visas, name="tourist_visas"),
+    path('long-term_visas', views.long_term_visas, name="long-term_visas"),
+    path('international_passport', views.international_passport, name="international_passport"),
+    path('invitations', views.invitations, name="invitations"),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -5,3 +5,20 @@ from django.shortcuts import render_to_response
 def main_page(request):
     args = {'username': auth.get_user(request).username}
     return render_to_response('main_page/main_page.html', args)
+
+
+def tourist_visas(request):
+    args = {'username': auth.get_user(request).username}
+    return render_to_response('main_page/tourist_visas.html', args)
+
+def long_term_visas(request):
+    args = {'username': auth.get_user(request).username}
+    return render_to_response('main_page/long_term_visas.html', args)
+
+def international_passport(request):
+    args = {'username': auth.get_user(request).username}
+    return render_to_response('main_page/international_passport.html', args)
+
+def invitations(request):
+    args = {'username': auth.get_user(request).username}
+    return render_to_response('main_page/invitations.html', args)
